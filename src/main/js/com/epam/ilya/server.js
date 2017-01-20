d3e185c1248eb789399f8eb9cf1f18e96aae8460
BondarenkoIlya <bondarenko_elijah@mail.ru>
2017-01-20 16:32:51 +0600

    Initial commit

--- src/main/js/com/epam/ilya/server.js
+++ src/main/js/com/epam/ilya/server.js
@@ -6,5 +6,5 @@ http.createServer(function(req, res) {
     file.serve(req, res);
 }).listen(8080);
 
-console.log('Server running on port 8080');
+console.log('Server running on port 8080');// look on GeekBrains Youtube chanel
 var name;
